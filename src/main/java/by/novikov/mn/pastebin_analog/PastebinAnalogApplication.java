5511836ba9f1ca7546cce77cc4a38a2f16fc2ccc
novikovmn <novikovmn93@gmail.com>
2025-12-09 20:23:34 +0300

    init

---
+++ src/main/java/by/novikov/mn/pastebin_analog/PastebinAnalogApplication.java
@@ -0,0 +1,13 @@
+package by.novikov.mn.pastebin_analog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastebinAnalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastebinAnalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/novikov/mn/pastebin_analog/PastebinAnalogApplicationTests.java
@@ -0,0 +1,13 @@
+package by.novikov.mn.pastebin_analog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastebinAnalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
