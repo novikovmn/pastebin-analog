082be46fe90681c92ff83341d18887fc7b9c6e00
novikovmn <novikovmn93@gmail.com>
2025-12-10 17:34:29 +0300

    add NotFoundEntityException

---
+++ src/main/java/by/novikov/mn/pastebin_analog/exception/NotFoundEntityException.java
@@ -0,0 +1,8 @@
+package by.novikov.mn.pastebin_analog.exception;
+
+public class NotFoundEntityException extends RuntimeException {
+
+    public NotFoundEntityException(String message) {
+        super(message);
+    }
+}
